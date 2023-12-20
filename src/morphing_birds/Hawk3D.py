4b9e944bb8683d0daf955c4741b05597f265558a
Lydia France <lfrance@turing.ac.uk>
2023-12-20 20:34:01 +0000

    Add tests for polygons

--- src/morphing_birds/Hawk3D.py
+++ src/morphing_birds/Hawk3D.py
@@ -43,6 +43,7 @@ class Hawk3D:
         ],
     }
 
+
     names_fixed_keypoints = [
         "left_shoulder", 
         "left_tailbase", 
@@ -59,6 +60,13 @@ class Hawk3D:
         "right_tailtip",
     ]
 
+    names_left_keypoints = [
+        "left_wingtip", 
+        "left_primary", 
+        "left_secondary",
+        "left_tailtip",
+    ]
+
     names_keypoints = [
         "left_wingtip",   "right_wingtip", 
         "left_primary",   "right_primary", 
@@ -66,32 +74,55 @@ class Hawk3D:
         "left_tailtip",   "right_tailtip"]
 
     def __init__(self, filename):
-        self.marker_names, self.keypoints = self.load_data(filename)
+        self.names_all_keypoints, self.all_keypoints = self.load_data(filename)
+        self.fixed_keypoints = self.get_keypoints_by_names(self.names_fixed_keypoints)
+        self.right_keypoints = self.get_keypoints_by_names(self.names_right_keypoints)
+        self.left_keypoints = self.get_keypoints_by_names(self.names_left_keypoints)
+
+        # Initialise polygons for plotting
+        self._init_polygons()
 
     def load_data(self,filename):
         with open(filename, 'r') as file:
             data = np.loadtxt(file, delimiter=',', skiprows=0, dtype='str')
         
-        # Get the marker names from the first row of the csv file
+        # Get the marker names from the first row of the csv file, get every 3rd name
         # and remove the '_x' from the names
-        marker_names = data[0].reshape(-1, 3)
-        marker_names = list(np.char.strip(marker_names[:, 0], '_x'))
+        names_all_keypoints = data[0].reshape(-1, 3)
+        names_all_keypoints = list(np.char.strip(names_all_keypoints[:, 0], '_x'))
 
         # Load marker coordinates and reshape to [n,3] matrix where n is the
         # number of markers
-        keypoints = data[1].astype(float)
-        keypoints = keypoints.reshape(-1, 3) # [n,3]
+        all_keypoints = data[1].astype(float)
+        all_keypoints = all_keypoints.reshape(-1, 3) # [n,3]
 
-        return marker_names, keypoints
+        return names_all_keypoints, all_keypoints
 
-    @staticmethod
-    def get_keypoints_by_names(marker_names, keypoints, names_to_find=None):
+    
+    def get_keypoints_by_names(self,names_to_find=None):
         """
         Returns the keypoints with the given names.
         """
+
+        marker_names = self.names_all_keypoints
+        keypoints = self.all_keypoints
+
         if names_to_find is None:
             names_to_find = marker_names
 
         indices = [marker_names.index(name) for name in names_to_find]
 
         return keypoints[indices]
+    
+
+    def _init_polygons(self):
+        """
+        Initialise polygons for plotting. 
+        Each polygon is created with the indices of vertices forming it.
+        These indices are used to subset the keypoint array when plotting.
+        """
+        self._polygons = {}
+        for section_name, keypoint_names in self.body_sections.items():
+            self._polygons[section_name] = self.get_keypoints_by_names(keypoint_names)
+
+    
--- tests/test_loading_data.py
+++ tests/test_loading_data.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 
 from morphing_birds import Hawk3D
 
@@ -8,14 +9,38 @@ def hawk3d_fixture():
     return Hawk3D(filename)
 
 def test_load_data(hawk3d_fixture):
-    assert hawk3d_fixture.marker_names is not None
-    assert hawk3d_fixture.keypoints is not None
+    assert hawk3d_fixture.names_all_keypoints is not None
+    assert hawk3d_fixture.all_keypoints is not None
 
 def test_marker_name_extraction(hawk3d_fixture):
     expected_first_marker_name = "left_secondary"
-    assert hawk3d_fixture.marker_names[0] == expected_first_marker_name
+    assert hawk3d_fixture.names_all_keypoints[0] == expected_first_marker_name
 
 def test_keypoint_initialisation(hawk3d_fixture):
-    assert hawk3d_fixture.keypoints.shape == (14, 3)
+    assert hawk3d_fixture.all_keypoints.shape == (14, 3)
+    assert hawk3d_fixture.fixed_keypoints.shape == (6, 3)
+    assert hawk3d_fixture.right_keypoints.shape == (4, 3)
+    assert hawk3d_fixture.left_keypoints.shape == (4, 3)
+
+def test_polygon(hawk3d_fixture):
+
+    # Get coords from head directly
+    expected_keypoints = ["right_shoulder", "hood", "left_shoulder"]
+    expected_coords = hawk3d_fixture.get_keypoints_by_names(expected_keypoints)
+
+    # Retrieve the coordinates used in the "head" polygon
+    head_polygon_coords = hawk3d_fixture._polygons["head"]
+
+    assert np.allclose(head_polygon_coords, expected_coords)
     
-    
+    # Get coords from left handwing directly
+    expected_keypoints = ["left_wingtip", "left_primary", "left_secondary"]
+    expected_coords = hawk3d_fixture.get_keypoints_by_names(expected_keypoints)
+
+    # Retrieve the coordinates used in the "left_handwing" polygon
+    left_handwing_polygon_coords = hawk3d_fixture._polygons["left_handwing"]
+
+    assert np.allclose(left_handwing_polygon_coords, expected_coords)
+
+
+
