692b23cb6df8e43ca1b60d8bbdeb8a302559589d
Lydia France <lfrance@turing.ac.uk>
2023-12-20 18:43:31 +0000

    started Hawk3D class definitions

---
+++ src/morphing_birds/Hawk3D.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+class Hawk3D:
+    body_sections = {
+        "left_handwing": [
+            "left_wingtip", 
+            "left_primary", 
+            "left_secondary",
+        ],
+        "right_handwing": [
+            "right_wingtip", 
+            "right_primary", 
+            "right_secondary",
+        ],
+        "left_armwing": [
+            "left_primary", 
+            "left_secondary", 
+            "left_tailbase", 
+            "left_shoulder",
+        ],
+        "right_armwing": [
+            "right_primary", 
+            "right_secondary", 
+            "right_tailbase",
+            "right_shoulder",
+        ],
+        "body": [
+            "right_shoulder", 
+            "left_shoulder", 
+            "left_tailbase", 
+            "right_tailbase",
+        ],
+        "head": [
+            "right_shoulder", 
+            "hood", 
+            "left_shoulder",
+        ],
+        "tail": [
+            "right_tailtip", 
+            "left_tailtip", 
+            "left_tailbase", 
+            "right_tailbase",
+        ],
+    }
+
+    names_fixed_keypoints = [
+        "left_shoulder", 
+        "left_tailbase", 
+        "right_tailbase", 
+        "right_shoulder",
+        "hood", 
+        "tailpack"
+    ]
+
+    names_right_keypoints = [
+        "right_wingtip", 
+        "right_primary", 
+        "right_secondary",
+        "right_tailtip",
+    ]
+
+    names_keypoints = [
+        "left_wingtip",   "right_wingtip", 
+        "left_primary",   "right_primary", 
+        "left_secondary", "right_secondary", 
+        "left_tailtip",   "right_tailtip"]
+
+    def __init__(self, filename):
+        self.marker_names, self.keypoints = self.load_data(filename)
+
+    def load_data(filename):
+        data = np.load(open(filename),
+                          delimiter=",",
+                          dtype='str',
+                          skiprows=0)
+
+        # Get the marker names from the first row of the csv file
+        # and remove the '_x' from the names
+        marker_names = data[0].reshape(-1, 3)
+        marker_names = list(np.char.strip(marker_names[:, 0], '_x'))
+
+        # Load marker coordinates and reshape to [n,3] matrix where n is the
+        # number of markers
+        keypoints = data[1].astype(float)
+        keypoints = keypoints.reshape(-1, 3) # [n,3]
+
+        return marker_names, keypoints
+
+    @staticmethod
+    def get_keypoints_by_names(marker_names, keypoints, names_to_find=None):
+        """
+        Returns the keypoints with the given names.
+        """
+        if names_to_find is None:
+            names_to_find = marker_names
+
+        indices = [marker_names.index(name) for name in names_to_find]
+
+        return keypoints[indices]
