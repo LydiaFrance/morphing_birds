2c90644b8ffa22611121f0271328eb0fa16798f5
Lydia France <lfrance@turing.ac.uk>
2023-12-24 18:26:00 +0000

    Added animation capabilities.

--- src/morphing_birds/Hawk3D.py
+++ src/morphing_birds/Hawk3D.py
@@ -5,6 +5,7 @@ from scipy.spatial.transform import Rotation as R
 import ipywidgets as widgets
 from IPython.display import display
 from IPython.display import clear_output
+from matplotlib.animation import FuncAnimation
 
 
 # ----- KeypointManager Class -----
@@ -151,6 +152,7 @@ class KeypointManager:
             print(keypoints.shape)
             raise ValueError("Keypoints missing.")
         
+
         return keypoints
 
     def _mirror_keypoints(self, keypoints):
@@ -222,6 +224,38 @@ class KeypointManager:
         # Update the right keypoints too.
         self.right_keypoints = self.get_keypoints_by_names(self.names_right_keypoints)
 
+
+    def validate_frames(self, keypoints_frames):
+        """
+        Validates the keypoints_frames and returns a list of keypoints for each frame.
+        """
+        if not isinstance(keypoints_frames, list):
+            keypoints_frames = [keypoints_frames]
+        for ii, keypoints in enumerate(keypoints_frames):
+
+            # The if statement is to ensure that all keypoint sequences have the same length
+            if ii > 0 and keypoints.shape[0] != keypoints_frames[ii - 1].shape[0]:
+                raise ValueError("All keypoint sequences must have the same length")
+            
+            keypoints_frames[ii] = self._validate_keypoints(keypoints)
+        return keypoints_frames
+    
+    def format_keypoints(self, keypoints):
+        """
+        Apply transforms on keypoints to make sure they are the correct shape.
+
+        - Reshaping of flat keypoint arrays into (T, L, 3) arrays, where T is
+            the number of frames and L the number of landmarks
+        """
+        nMarkers = len(self.names_keypoints)
+
+        if len(keypoints.shape) == 1:
+            keypoints = keypoints.reshape((-1, len(BirdData.landmarks), 3))
+        if keypoints.shape[0] == 1:
+            keypoints = keypoints.reshape((len(BirdData.landmarks), 3))
+
+
+        return keypoints
 
 
 class HawkPlotter:
@@ -406,7 +440,7 @@ class HawkPlotter:
         plt.ion()  # Turn on interactive mode
         
         az_slider = widgets.IntSlider(min=-90, max=90, step=5, value=az, description='azimuth')
-        el_slider = widgets.IntSlider(min=-90, max=90, step=5, value=el, description='elevation')
+        el_slider = widgets.IntSlider(min=-15, max=90, step=5, value=el, description='elevation')
 
         plot_output = widgets.Output()
 
@@ -434,17 +468,6 @@ class HawkPlotter:
         az_slider.observe(update_plot, names='value')
         el_slider.observe(update_plot, names='value')
 
-       # Initialise the plot
-        # self.plot(keypoints=keypoints,
-        #                 fig=fig,
-        #                 ax=ax,
-        #                 el=el_slider.value,
-        #                 az=az_slider.value,
-        #                 colour=colour,
-        #                 alpha=alpha,
-        #                 horzDist=horzDist,
-        #                 bodypitch=bodypitch,
-        #                 vertDist=vertDist)     
         
 
         # Display the sliders
@@ -525,10 +548,173 @@ class HawkAnimator:
     def __init__(self, plotter):
         self.plotter = plotter
 
-    def animate(self):
-        self.plotter.plot()
+    def _format_keypoint_frames(self, keypoints_frames):
 
-        pass
+        if len(np.shape(keypoints_frames)) == 2:
+            keypoints_frames = keypoints_frames.reshape(1, -1, 3)
+            print("Warning: Only one frame given.")
+
+    # Mirror the keypoints_frames if only the right is given. 
+        if keypoints_frames.shape[1] == len(self.plotter.keypoint_manager.names_right_keypoints):
+            keypoints_frames = self.plotter.keypoint_manager._mirror_keypoints(keypoints_frames)
+
+        return keypoints_frames
+        
+    def animate(self, 
+                keypoints_frames, 
+                fig=None, 
+                ax=None, 
+                rotation_type="static", 
+                el=20, 
+                az=60, 
+                alpha=0.3, 
+                colour=None, 
+                horzDist_frames=None, 
+                bodypitch_frames=None):
+        """
+        Create an animated 3D plot of a hawk video.
+        """
+
+        # Mirror the keypoints if only the right is given.
+        keypoints_frames = self._format_keypoint_frames(keypoints_frames)
+
+        # Find the number of frames 
+        num_frames = keypoints_frames.shape[0]
+
+
+        # Initialize figure and axes
+        if ax is None:
+            fig, ax = self.plotter.get_plot3d_view(fig)
+
+        # Prepare camera angles
+        el_frames, az_frames = self.get_camera_angles(num_frames=num_frames, 
+                                                      rotation_type=rotation_type, 
+                                                      el=el, 
+                                                      az=az)
+        
+        # Check if the horzDist_frames is given, if so check it is the correct length
+        if horzDist_frames is not None:
+            if len(horzDist_frames) != num_frames:
+                raise ValueError("horzDist_frames must be the same length as keypoints_frames.")
+            
+        # Check if the bodypitch_frames is given, if so check it is the correct length
+        if bodypitch_frames is not None:
+            if len(bodypitch_frames) != num_frames:
+                raise ValueError("bodypitch_frames must be the same length as keypoints_frames.")
+            
+        # Plot settings
+        self.plotter._plot_settings(ax, horzDist=0 if horzDist_frames is None else horzDist_frames)
+
+        # Update function for animation
+        def update_animated_plot(frame, *fargs):
+            fig, ax, keypoints, el_frames, az_frames, alpha, colour, horzDist_frames, bodypitch_frames = fargs
+            ax.clear()
+            # Here, you need to adjust how keypoints for the current frame are passed to plot
+            self.plotter.plot(keypoints=keypoints[frame], 
+                              fig=fig, 
+                              ax=ax, 
+                              el=el_frames[frame], 
+                              az=az_frames[frame], 
+                              alpha=alpha, 
+                              colour=colour, 
+                              horzDist=horzDist_frames[frame] if horzDist_frames else None, 
+                              bodypitch=bodypitch_frames[frame] if bodypitch_frames else None)
+            return fig, ax
+
+        # Creating the animation
+        animation = FuncAnimation(fig, update_animated_plot, 
+                                  frames=num_frames, 
+                                  fargs=(fig, ax, keypoints_frames, el_frames, az_frames, alpha, colour, horzDist_frames, bodypitch_frames), 
+                                  interval=20, repeat=False)
+        
+        return animation
+
+    def get_camera_angles(self,num_frames, rotation_type, el=20, az=60):
+        """
+        Creates two arrays of camera angles for the length of the animation.
+
+        "static" -- the angles do not change in the animation, set using el, az.
+
+        "dynamic" -- fast rotations
+
+        "slow" -- slow rotations
+
+        Used in: Creates inputs for plot_hawk3D_frame
+        """
+
+        if el is None or az is None:
+            return [None, None]
+
+        def linspacer(number_array, firstValue, endValue, nFrames):
+            """
+            Function to make linearly spaced numbers between two values of a
+            given length.
+            """
+
+            number_array = np.append(
+                number_array, np.linspace(firstValue, endValue, nFrames))
+            return number_array
+
+        if "dynamic" in rotation_type:
+
+            tenthFrames = round(num_frames * 0.1)
+            remainderFrames = num_frames - (tenthFrames * 9)
+
+            az_frames = np.linspace(40, 40, tenthFrames)
+            az_frames = linspacer(az_frames, 40, 10, tenthFrames)
+            az_frames = linspacer(az_frames, 10, 10, tenthFrames)
+            az_frames = linspacer(az_frames, 10, 90, tenthFrames)
+            az_frames = linspacer(az_frames, 90, 90, tenthFrames * 2)
+            az_frames = linspacer(az_frames, 90, -90, tenthFrames)
+            az_frames = linspacer(az_frames, -90, -90, tenthFrames)
+            az_frames = linspacer(az_frames, -90, 40, tenthFrames)
+            az_frames = linspacer(az_frames, 40, 40, remainderFrames)
+
+            el_frames = np.linspace(20, 20, tenthFrames)
+            el_frames = linspacer(el_frames, 20, 15, tenthFrames)
+            el_frames = linspacer(el_frames, 15, 0, tenthFrames)
+            el_frames = linspacer(el_frames, 0, 0, tenthFrames)
+            el_frames = linspacer(el_frames, 0, 80, tenthFrames)
+            el_frames = linspacer(el_frames, 80, 80, tenthFrames)
+            el_frames = linspacer(el_frames, 80, 15, tenthFrames)
+            el_frames = linspacer(el_frames, 15, 15, tenthFrames)
+            el_frames = linspacer(el_frames, 15, 20, tenthFrames)
+            el_frames = linspacer(el_frames, 20, 20, remainderFrames)
+
+        elif "slow" in rotation_type:
+
+            halfFrames = round(num_frames * 0.5)
+            tenthFrames = round(num_frames * 0.1)
+            remainderFrames = num_frames - (halfFrames + (tenthFrames * 2) +
+                                            tenthFrames + tenthFrames)
+
+            az_frames = np.linspace(90, 90, halfFrames)
+            az_frames = linspacer(az_frames, 90, -90,
+                                  tenthFrames)  # Switch to back
+            az_frames = linspacer(az_frames, -90, -90, tenthFrames * 2)
+            az_frames = linspacer(az_frames, -90, 90,
+                                  tenthFrames)  # Switch to front
+            az_frames = linspacer(az_frames, 90, 90, remainderFrames)
+
+            remainderFrames = num_frames - (tenthFrames * 9)
+
+            el_frames = np.linspace(80, 80, tenthFrames * 2)
+            el_frames = linspacer(el_frames, 80, 20,
+                                  tenthFrames)  # Transition to lower
+            el_frames = linspacer(el_frames, 20, 20, tenthFrames * 2)
+            el_frames = linspacer(el_frames, 20, 10,
+                                  tenthFrames)  # Switch to back
+            el_frames = linspacer(el_frames, 10, 10, tenthFrames * 3)
+            el_frames = linspacer(el_frames, 10, 80,
+                                  remainderFrames)  # Switch to front
+
+        else:
+            el_frames = np.linspace(el, el, num_frames)
+            az_frames = np.linspace(az, az, num_frames)
+
+        return el_frames, az_frames
+
+
 
 # ----- Hawk3D Class -----
 
@@ -543,7 +729,7 @@ class Hawk3D:
         self.plotter = HawkPlotter(self.keypoint_manager)
         self.animator = HawkAnimator(self.plotter)
 
-    def display_hawk(self, user_keypoints=None):
+    def display_hawk(self, user_keypoints=None, el = 20, az = 60):
         
         """
         Displays the hawk using either the default keypoints or user-provided keypoints.
@@ -563,7 +749,14 @@ class Hawk3D:
         self.plotter.interactive_plot()
 
 
-    def animate_hawk(self, keypoint_sequence):
+    def animate_hawk(self, keypoint_sequence,
+                rotation_type="static", 
+                el=20, 
+                az=60, 
+                alpha=0.3, 
+                colour=None, 
+                horzDist_frames=None, 
+                bodypitch_frames=None):
         """
         Animates the hawk using a sequence of keypoints.
 
@@ -571,12 +764,16 @@ class Hawk3D:
         keypoint_sequence (numpy.ndarray): An array of keypoints for each frame.
                                            Expected shape is [n, 4, 3], where n is the number of frames.
         """
-        for frame_keypoints in keypoint_sequence:
-            # Update the keypoints in the plotter for the current frame
-            self.plotter.update_keypoints(frame_keypoints)
             
-            # Render the plot for the current frame
-            self.plotter.plot()
+        # Use the animator to animate the hawk with the given keypoints.
+        self.animation = self.animator.animate(keypoint_sequence,
+                                               rotation_type=rotation_type, 
+                                               el=el, 
+                                               az=az, 
+                                               alpha=alpha, 
+                                               colour=colour, 
+                                               horzDist_frames=horzDist_frames, 
+                                               bodypitch_frames=bodypitch_frames)
 
 
 
