18f78af0ccc76f357aadf3e8539f2d3cadd808dd
Lydia France <lfrance@turing.ac.uk>
2024-04-18 10:50:26 +0100

    BROKEN STATE: the animations of PCA don't look right

--- src/morphing_birds/__init__.py
+++ src/morphing_birds/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 from .Hawk3D import Hawk3D, plot, interactive_plot, animate
 # from .HawkData import HawkData
-from .HawkPCA import process_data, filter_by, run_PCA, HawkPCATest
+from .HawkPCA import process_data, filter_by, run_PCA, get_score_range, reconstruct
 # from .PCAFigures import PCAFigures
 # from .trytofix import 
 
