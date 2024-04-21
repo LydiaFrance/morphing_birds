ef1569716adf6be8e54e506cdbbc942293e9a6d2
Lydia France <lfrance@turing.ac.uk>
2024-04-21 21:03:20 +0100

    removing non morphing files

--- src/morphing_birds/__init__.py
+++ src/morphing_birds/__init__.py
@@ -4,8 +4,8 @@ morphing_birds: Run PCA on morphing bird wings and tail in flight.
 from __future__ import annotations
 
 from .Hawk3D import Hawk3D, plot, interactive_plot, animate
-from .HawkPCA import process_data, filter_by, run_PCA, get_score_range, reconstruct
-from .PCAFigures import plot_components_grid
+from ..reference.HawkPCA import process_data, filter_by, run_PCA, get_score_range, reconstruct
+from ..reference.PCAFigures import plot_components_grid
 # from .trytofix import 
 
 # from .Figures import plot_components
