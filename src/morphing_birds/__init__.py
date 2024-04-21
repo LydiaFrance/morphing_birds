d9839726764c99b9c69d8679867e876f07ac65de
Lydia France <lfrance@turing.ac.uk>
2024-04-21 23:04:52 +0100

    Include a tutorial

--- src/morphing_birds/__init__.py
+++ src/morphing_birds/__init__.py
@@ -4,12 +4,6 @@ morphing_birds: Run PCA on morphing bird wings and tail in flight.
 from __future__ import annotations
 
 from .Hawk3D import Hawk3D, plot, interactive_plot, animate
-from ..reference.HawkPCA import process_data, filter_by, run_PCA, get_score_range, reconstruct
-from ..reference.PCAFigures import plot_components_grid
-# from .trytofix import 
-
-# from .Figures import plot_components
-
 
 __all__ = ("trytofix",
            "__version__")
