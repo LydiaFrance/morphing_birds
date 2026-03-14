72441f6cd668368919622871f6a88733ac228961
Lydia France <lydia.france@biology.ox.ac.uk>
2026-03-14 10:04:00 +0000

    Update package init and bump version to 0.2.0
    Rewrite __init__.py to export the new module structure. Add pyyaml
    dependency to pyproject.toml. Bump version to 0.2.0.

--- src/morphing_birds/__init__.py
+++ src/morphing_birds/__init__.py
@@ -1,43 +1,86 @@
-"""
-morphing_birds: Run PCA on morphing bird wings and tail in flight.
-"""
+"""morphing_birds: Config-driven 3D animal morphology toolkit."""
+
 from __future__ import annotations
 
-from .SkeletonDefinition import SkeletonDefinition
-from .Animal3D import Animal3D
-from .ArbitraryBird3D import ArbitraryBird3D
-from .hawk_skeleton_definition import HawkSkeletonDefinition
-from .spider_skeleton_definition import SpiderSkeletonDefinition
-from .kestrel_skeleton_definition import KestrelSkeletonDefinition
-from .pigeon_skeleton_definition import PigeonSkeletonDefinition
-from .Hawk3D import Hawk3D
-from .Spider3D import Spider3D
-from .Kestrel3D import Kestrel3D
-from .Pigeon3D import Pigeon3D
+from .skeleton import SkeletonDefinition
+from .animal import Animal3D
+from .bilateral import (
+    mirror_to_bilateral,
+    make_unilateral,
+    make_bilateral,
+    validate_left_right,
+)
+from .transforms import TransformState
+from .scaling import (
+    unit_conversion_factor,
+    compute_wingspan,
+    compute_body_length,
+    UNIT_FACTORS,
+)
+from .data_loading import (
+    load_from_csv,
+    load_from_dataframe,
+    load_from_dict,
+    load_mean_shape_csv,
+)
+from .configs import load_config, list_configs
 from .plotting import (
-    plot, 
-    interactive_plot, 
-    plot_multiple, 
+    plot,
+    interactive_plot,
+    plot_multiple,
     plot_plotly,
     plot_plotly_compare,
     plot_compare_plotly,
     plot_partial_plotly,
     plot_plotly_with_trace,
-    animate, 
-    animate_compare, 
-    animate_plotly, 
+    animate,
+    animate_compare,
+    animate_plotly,
     animate_plotly_compare,
     animate_plotly_partial,
     save_plotly_animation,
-    create_dash_app
+    create_dash_app,
 )
 
-__all__ = ("__version__", 
-           "SkeletonDefinition", "Animal3D", "ArbitraryBird3D",
-           "Hawk3D", "HawkSkeletonDefinition", "Kestrel3D", "KestrelSkeletonDefinition",
-           "Pigeon3D", "PigeonSkeletonDefinition", "SpiderSkeletonDefinition", "Spider3D",
-           "plot", "interactive_plot", "plot_multiple", "plot_plotly", "plot_plotly_compare", "plot_partial_plotly",
-           "plot_compare_plotly", "animate", "animate_compare", "create_dash_app", "plot_plotly_with_trace",
-           "animate_plotly", "animate_plotly_compare", "save_plotly_animation",
-           "animate_plotly_partial")
-__version__ = "0.1.0"
+__all__ = (
+    "__version__",
+    # Core
+    "SkeletonDefinition",
+    "Animal3D",
+    "TransformState",
+    # Bilateral
+    "mirror_to_bilateral",
+    "make_unilateral",
+    "make_bilateral",
+    "validate_left_right",
+    # Scaling
+    "unit_conversion_factor",
+    "compute_wingspan",
+    "compute_body_length",
+    "UNIT_FACTORS",
+    # Data loading
+    "load_from_csv",
+    "load_from_dataframe",
+    "load_from_dict",
+    "load_mean_shape_csv",
+    # Config
+    "load_config",
+    "list_configs",
+    # Plotting
+    "plot",
+    "interactive_plot",
+    "plot_multiple",
+    "plot_plotly",
+    "plot_plotly_compare",
+    "plot_compare_plotly",
+    "plot_partial_plotly",
+    "plot_plotly_with_trace",
+    "animate",
+    "animate_compare",
+    "animate_plotly",
+    "animate_plotly_compare",
+    "animate_plotly_partial",
+    "save_plotly_animation",
+    "create_dash_app",
+)
+__version__ = "0.2.0"
